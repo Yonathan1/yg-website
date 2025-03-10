fb3194fe4cd3af66202967141fdf2be0de3b226c
Yonathan1 <nathan.gunawan93@gmail.com>
2025-03-10 10:28:47 +0700

    Update sub section styling

--- src/components/heroSection/SubHeroSection.jsx
+++ src/components/heroSection/SubHeroSection.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const SubHeroSection = () => {
   return (
-    <div className='w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl py-8 items-center gap-4 bg-darkGrey'>
+    <div className='w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl sm:text-4xl py-8 items-center gap-4 bg-darkGrey'>
         <p>Leadership</p>
-        <p>Optimization</p>
-        <p>Business Alignment</p>
+        <p className='md:block sm: hidden'>Optimization</p>
+        <p className='md:block sm: hidden'>Business Alignment</p>
     </div>
   )
 }

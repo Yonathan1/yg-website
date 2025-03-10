5672be594026488b1cf6219c922d71cf11bac8ee
Yonathan1 <nathan.gunawan93@gmail.com>
2025-03-10 10:45:26 +0700

    Add hyperlink

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -6,8 +6,8 @@ import { FaLinkedinIn } from 'react-icons/fa';
 const ContactSocial = () => {
   return (
     <div className='flex gap-4'>
-        <SingleContactSocial link="#" Icon={FaLinkedinIn} />
-        <SingleContactSocial link="#" Icon={FiGithub} />
+        <SingleContactSocial link="https://www.linkedin.com/in/yonathan-gunawan-25447995/" Icon={FaLinkedinIn} />
+        <SingleContactSocial link="https://github.com/Yonathan1/" Icon={FiGithub} />
     </div>
   )
 }

61a5032db190f7a6e453b28b4d7749efd57caf6e
Yonathan1 <nathan.gunawan93@gmail.com>
2025-03-14 15:17:07 +0700

    Update email logo size

--- src/components/contactMeSection/ContactMeRight.jsx
+++ src/components/contactMeSection/ContactMeRight.jsx
@@ -5,7 +5,7 @@ import ContactSocial from './ContactSocial'
 const ContactMeRight = () => {
   return (
     <div className='flex flex-col items-center justify-center gap-12'>
-        <img src='../images/email-image.png' alt='Contact me' className='max-w-[300px]'/>
+        <img src='../images/email-image.png' alt='Contact me' className='sm:max-w-[150px] md:max-w-[300px]'/>
         <ContactInfo />
         <ContactSocial />
     </div>

c0cea2d949b2b1e21b9b6d6bc4f2b2575d5b1527
Yonathan1 <nathan.gunawan93@gmail.com>
2025-03-14 15:20:42 +0700

    Update about me wording

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -4,7 +4,7 @@ const AboutMeText = () => {
   return (
     <div className='flex flex-col md:items-start sm:items-center md:text-left sm:text-center'>
       <h2 className='text-6xl text-cyan mb-10'>About Me</h2>
-      <p className='text-white'>I help fintech and SaaS companies scale efficiently, optimize costs, and build high-impact products. With over 12 years of experience leading product and engineering teams, I specialize in product innovation, cloud cost reduction, and strategic execution that drive business growth.</p>
+      <p className='text-white'>I help Tech companies scale efficiently, optimize costs, and build high-impact products. With over 12 years of experience leading product and engineering teams, I specialize in product innovation, cloud cost reduction, and strategic execution that drive business growth.</p>
     </div>
   )
 }

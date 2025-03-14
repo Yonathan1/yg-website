45327988ee4090f9a45b26ac97cbd1a8b5a8b63a
Yonathan1 <nathan.gunawan93@gmail.com>
2025-03-14 12:58:15 +0700

    Add div

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -3,12 +3,16 @@
  const NavbarLogo = () => {
    return (
      <div>
-        <h1 className='text-white text-2xl sm:hidden md:block'>
-            Yonathan Gunawan
-        </h1>
-        <h1 className='text-white font-special font-extrabold text-4xl sm:block md:hidden'>
-            YG
-        </h1>
+        <div className='sm:hidden md:block'>
+          <h1 className='text-white text-2xl'>
+              Yonathan Gunawan
+          </h1>
+        </div>
+        <div className='sm:block md:hidden'>
+          <h1 className='text-white font-special font-extrabold text-4xl'>
+              YG
+          </h1>
+        </div>
      </div>
    )
  }

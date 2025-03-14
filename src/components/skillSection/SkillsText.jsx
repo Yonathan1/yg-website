eb12d721b8ed08f79e0492988e464e4ddf547176
Yonathan1 <nathan.gunawan93@gmail.com>
2025-03-14 14:08:16 +0700

    Update skill wording

--- src/components/skillSection/SkillsText.jsx
+++ src/components/skillSection/SkillsText.jsx
@@ -5,7 +5,7 @@ const SkillsText = () => {
     <div className='flex flex-col items-center mt-[100px]'>
         <h2 className='text-6xl text-cyan mb-10'>My Skills</h2>
         <p className='text-lg text-center text-white'>
-            I have been working with all these skills professionally and personally to build my portfolio projects.
+            I have been working with all these skills professionally and personally.
         </p>
     </div>
   )

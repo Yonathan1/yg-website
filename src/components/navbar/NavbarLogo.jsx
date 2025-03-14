48bd2be00b470d3dc32589b8a3e9211fd4ca8979
Yonathan1 <nathan.gunawan93@gmail.com>
2025-03-14 12:56:08 +0700

    Update styling

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -3,7 +3,7 @@
  const NavbarLogo = () => {
    return (
      <div>
-        <h1 className='text-white text-2xl max-w-0 sm:max-w-full overflow-hidden md:block'>
+        <h1 className='text-white text-2xl sm:hidden md:block'>
             Yonathan Gunawan
         </h1>
         <h1 className='text-white font-special font-extrabold text-4xl sm:block md:hidden'>
